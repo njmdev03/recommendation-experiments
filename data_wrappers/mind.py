8d3d6f1f6a96ccaa8aedc8137c61714a31523306
njmdev03 <njmdev03@gmail.com>
2026-05-01 02:25:28 -0500

    Reformat dataset output

--- data_wrappers/mind.py
+++ data_wrappers/mind.py
@@ -1,39 +1,39 @@
 import pandas as pd
 
 
-class MINDNews:
-    def __init__(self, split="train"):
-        news_cols = ['news_id', 'category', 'subcategory', 'title',
-                     'abstract', 'url', 'title_entities', 'abstract_entities']
-        self.news_df = pd.read_csv('data/MINDsmall_train/news.tsv',
-                                    sep='\t', names=news_cols)
+# class MINDNews:
+#     def __init__(self, split="train"):
+#         news_cols = ['news_id', 'category', 'subcategory', 'title',
+#                      'abstract', 'url', 'title_entities', 'abstract_entities']
+#         self.news_df = pd.read_csv('data/MINDsmall_train/news.tsv',
+#                                     sep='\t', names=news_cols)
 
-        self.news_df = self.news_df.set_index("news_id")
-        self.news_df = self.news_df.fillna("")
+#         self.news_df = self.news_df.set_index("news_id")
+#         self.news_df = self.news_df.fillna("")
 
-    def __len__(self):
-        return self.news_df.__len__()
+#     def __len__(self):
+#         return self.news_df.__len__()
 
-    def __getitem__(self, key):
-        return self.news_df.loc[key]
+#     def __getitem__(self, key):
+#         return self.news_df.loc[key]
 
-    def iter(self, col_id):
-        return self.news_df[col_id]
+#     def iter(self, col_id):
+#         return self.news_df[col_id]
 
-class MINDBehaviors:
-    def __init__(self, split="train"):
-        beh_cols = ['impression_id', 'user_id', 'time', 'history', 'impressions']
-        self.behaviors_df = pd.read_csv('data/MINDsmall_train/behaviors.tsv',
-                                        sep='\t', names=beh_cols)
+# class MINDBehaviors:
+#     def __init__(self, split="train"):
+#         beh_cols = ['impression_id', 'user_id', 'time', 'history', 'impressions']
+#         self.behaviors_df = pd.read_csv('data/MINDsmall_train/behaviors.tsv',
+#                                         sep='\t', names=beh_cols)
 
-    def __len__(self):
-        return self.behaviors_df.__len__()
+#     def __len__(self):
+#         return self.behaviors_df.__len__()
 
-    def __getitem__(self, idx):
-        return self.behaviors_df.loc[idx]
+#     def __getitem__(self, idx):
+#         return self.behaviors_df.loc[idx]
 
 class MINDDataset:
-    def __init__(self, news_tokenizer, split="train"):
+    def __init__(self, news_tokenizer=None, split="train", size = "small", no_hist_tok="<no_his>"):
         news_cols = ['news_id', 'category', 'subcategory', 'title',
                      'abstract', 'url', 'title_entities', 'abstract_entities']
         self.news_df = pd.read_csv('data/MINDsmall_train/news.tsv',
@@ -45,14 +45,36 @@ class MINDDataset:
 
         self.news_df = self.news_df.set_index("news_id")
         self.news_df = self.news_df.fillna("")
-        self.behaviors_df = self.behaviors_df.fillna("")
+        self.behaviors_df = self.behaviors_df.fillna(no_hist_tok)
 
         self.tok = news_tokenizer
 
     def __len__(self):
         return self.behaviors_df.__len__()
 
+    def get_news_ids(self):
+        return self.news_df.index.to_numpy()
+
+    def get_news_text(self, news_id, tokenize=True):
+        row = self.news_df.loc[news_id]
+
+        text = row["title"] + " " + row["abstract"]
+
+        if tokenize:
+            return self.tok.encode(text)
+        else:
+            return text
+
     def __getitem__(self, idx):
+        beh = self.behaviors_df.loc[idx]
+
+        hist_ids = beh.loc["history"].split(" ")
+        impressions = beh.loc["impressions"].split(" ")
+        cand_ids = [str(x).split("-")[0] for x in impressions]
+        labels = [str(x).split("-")[1] for x in impressions]
+
+        return hist_ids, cand_ids, labels
+
         beh = self.behaviors_df.loc[idx]
 
         # Get the impressions array
